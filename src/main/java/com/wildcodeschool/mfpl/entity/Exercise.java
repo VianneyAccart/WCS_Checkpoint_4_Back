b4749a8c1f181f57f23f3c057bc602d1911cd339
Vianney Accart <vianneyaccart@gmail.com>
2022-03-21 12:46:45 +0100

    initial commit

---
+++ src/main/java/com/wildcodeschool/mfpl/entity/Exercise.java
@@ -0,0 +1,57 @@
+package com.wildcodeschool.mfpl.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(min = 1, max = 1500)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "muscular_group_id")
+    private MuscularGroup muscularGroup;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MuscularGroup getMuscularGroup() {
+        return muscularGroup;
+    }
+
+    public void setMuscularGroup(MuscularGroup muscularGroup) {
+        this.muscularGroup = muscularGroup;
+    }
+}
---
+++ src/main/java/com/wildcodeschool/mfpl/entity/MuscularGroup.java
@@ -0,0 +1,45 @@
+package com.wildcodeschool.mfpl.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class MuscularGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 1, max = 150)
+    private String name;
+
+    @OneToMany(mappedBy = "muscularGroup")
+    private List<Exercise> exercises;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+}
