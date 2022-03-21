3e29e73c35d5c0d0fb28395cc61a6ea8171f5cb1
Vianney Accart <vianneyaccart@gmail.com>
2022-03-21 12:33:19 +0100

    initial commit

---
+++ src/main/java/com/wildcodeschool/mfpl/MfplApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.mfpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/mfpl/MfplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.mfpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
